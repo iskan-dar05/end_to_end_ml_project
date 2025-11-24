c4cb76c8a249e489ed3e82177063b73ee301307e
iskan-dar05 <skous2034@gmail.com>
2025-11-24 16:40:31 +0100

    termyx commit

--- EDA.py
+++ EDA.py
@@ -13,8 +13,9 @@ from sklearn.preprocessing import StandardScaler
 
 
 dst_folder = Path('dataset')
+plot_dir = Path('plot_images')
 dst_folder.mkdir(parents=True, exist_ok=True)
-
+plot_dir.mkdir(parents=True, exist_ok=True)
 path_str = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
 
 path = Path(path_str)
@@ -30,7 +31,7 @@ else:
     final_path = dst_folder / csv_file
     shutil.move(str(csv_path), str(final_path))
     print(f"Moving file from {csv_path} to {final_path}")
-    
+
 
 
 
@@ -43,7 +44,28 @@ print(df.info())
 print(df.describe())
 
 
+ax = df['Class'].value_counts().plot(kind="bar")
 
+plt.title("Fraud vs Non-Fraud Count")
+plt.xlabel("Class (0 = Non-Fraud, 1 = Fraud)")
+plt.ylabel("Number of Transactions")
+
+plt.savefig(plot_dir / 'barplot.png')
+
+plt.close()
+
+
+X = df.drop('Class', axis=1)
+y = df['Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+
+X_train[["Amount", "Time"]] = scaler.fit_transform(X_train[["Amount", "Time"]])
+X_test[["Amount", "Time"]] = scaler.transform(X_test[["Amount", "Time"]])
+
+sm = SMOTE(random_state=42)
 
 
 
